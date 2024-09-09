8d50f63b6b8b4e9bb88ab913398d23be60fc0451
RavuriBramhani <2320030104@klh.edu.in>
2024-09-09 21:21:37 +0530

    Add files via upload

---
+++ BirdManagement/src/com/bird/Bird.java
@@ -0,0 +1,9 @@
+package com.bird;
+
+public class Bird {
+	
+	public void fly() {
+        System.out.println("The bird is flying.");
+    }
+
+}
---
+++ BirdManagement/src/com/bird/Ostrich.java
@@ -0,0 +1,9 @@
+package com.bird;
+
+public class Ostrich extends Bird {
+	
+	 public void fly() {
+	        throw new UnsupportedOperationException("Ostriches cannot fly.");
+	    }
+
+}
