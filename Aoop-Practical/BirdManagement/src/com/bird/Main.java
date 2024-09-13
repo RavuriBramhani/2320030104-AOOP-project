62e5756578def1bf0d4a9621e04b58ae552e5082
RavuriBramhani <2320030104@klh.edu.in>
2024-09-13 13:56:02 +0530

    Add files via upload

---
+++ Aoop-Practical/BirdManagement/src/com/bird/Main.java
@@ -0,0 +1,19 @@
+package com.bird;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Bird bird = new Bird();
+        bird.fly(); 
+  
+        Bird ostrich = new Ostrich();
+        try {
+            ostrich.fly(); 
+        } 
+        catch (UnsupportedOperationException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+	}
+
+}
